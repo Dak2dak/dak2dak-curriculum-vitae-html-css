b75f6782921a3abfaaf4b62a7d53460d0aaa2f4b
Didier Dakoury <dazibao76@gmail.com>
2022-02-14 16:20:38 +0000

    toggle

---
+++ script.js
@@ -0,0 +1,25 @@
+const toggle = document.getElementById('btn');
+const swithBtn = document.getElementById('swithBtn');
+
+
+const language1 = document.getElementById('french__version');
+const language2 = document.getElementById('english__version');
+const title = document.getElementById('big-title');
+
+
+swithBtn.addEventListener('click', function Switching() {
+    
+    title.innerHTML = "Curriculum Vitæ";
+    language1.classList.toggle('isVisible');
+
+})
+
+toggle.addEventListener('click', function myFunction() {
+
+    title.innerHTML = "Resume";
+    language2.classList.toggle('isVisible');
+    // document.getElementById('swithBtn').disabled = true;
+    
+})
+
+
